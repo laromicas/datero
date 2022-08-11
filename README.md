5f99be56607db5b6d7ef459d4ca363e32a08db89
Lacides Miranda <laromicas@hotmail.com>
2022-08-10 20:11:17 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Datero
+Datero is a WIP Python command line tool to download and organize your Dat Roms.
+As today the tool supports dat-omatic, redump, and translated-english dats.
+It merges all the dats in a tree folder structure thought to use with Emulators rather than dats.
+
+## Installation
+<!--- ### TODO
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+```bash
+pip install foobar
+```
+-->
+
+Download a release and install requirements (recommended to use a virtual environment)
+```bash
+pip install -r requirements.txt
+```
+
+
+## Usage
+
+```bash
+# fill the json database with initial data
+$ ./seed
+
+# Download dats to tmp folder
+$ lib/repos/<dattype>/update -f
+$ lib/repos/nointro/update -f   # be careful when updating dats from datomatic, sometimes they put a captcha, and you may be banned if the cpatcha fails, captcha support is OTW
+
+# process dats and organize them
+lib/repos/<dattype>/update -p
+
+# process dats filtered by word
+lib/repos/<dattype>/update -p -d <filter>
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- lib/repos/nointro/download.py
+++ lib/repos/nointro/download.py
@@ -29,34 +29,6 @@ def sleep_time():
     time.sleep(random.randint(1, 6))
 
 
-
-# def waitUntilDownloadCompleted(maxTime=600):
-#     driver.execute_script("window.open()")
-#     # switch to new tab
-#     driver.switch_to.window(driver.window_handles[-1])
-#     # navigate to chrome downloads
-#     # driver.get('chrome://downloads')
-#     driver.get('about:downloads')
-#     # define the endTime
-#     endTime = time.time() + maxTime
-#     while True:
-#         try:
-#             # get the download percentage
-#             downloadPercentage = driver.execute_script(
-#                 "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
-#             # check if downloadPercentage is 100 (otherwise the script will keep waiting)
-#             if downloadPercentage == 100:
-#                 # exit the method once it's completed
-#                 return downloadPercentage
-#         except:
-#             pass
-#         # wait for 1 second before checking the percentage next time
-#         time.sleep(1)
-#         # exit method if the download not completed with in MaxTime.
-#         if time.time() > endTime:
-#             break
-
-
 def is_download_finished() -> bool:
     firefox_temp_file = sorted(Path(TMP_FOLDER).glob('*.part'))
     chrome_temp_file = sorted(Path(TMP_FOLDER).glob('*.crdownload'))
